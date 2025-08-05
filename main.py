5239aa730faa1a7ec3072ea9cf46e34489da3026
JOKERLinux4 <163691846+JOKERLinux4@users.noreply.github.com>
2025-08-06 00:21:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-main.py
+from pyrogram import Client
+import asyncio
+import os
 
-from pyrogram import Client, filters import os
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+SESSION = os.environ.get("SESSION")
 
-API_ID = int(os.environ.get("API_ID")) API_HASH = os.environ.get("API_HASH") SESSION = os.environ.get("SESSION")
+DEVELOPER_NAME = "قِٰـِۢــآئــد آلِٰـِۢــشِٰـِۢــيِٰـِۢـآطِٰـِۢـيِٰـِـنَ"
+DEVELOPER_USER = "@X_5NN"
+SOURCE_CHANNEL = "@C_9KK"
 
-DEVELOPER_NAME = "قِٰـِۢــآئــد آلِٰـِۢــشِٰـِۢــيِٰـِۢـآطِٰـِۢـيِٰـِـنَ" DEVELOPER_USER = "@X_5NN" SOURCE_CHANNEL = "@C_9KK"
+async def main():
+    async with Client(name="mybot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION) as app:
+        me = await app.get_me()
+        print(f"✅ تم تشغيل الجلسة بنجاح: {me.first_name} (@{me.username})")
 
-app = Client(name=SESSION, api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
-
-@app.on_message(filters.command("start") & filters.private) def start(client, message): message.reply_text( f"👋 مرحبًا بك في سورس {DEVELOPER_NAME}\n" f"👤 المطور: {DEVELOPER_USER}\n" f"📣 القناة: {SOURCE_CHANNEL}" )
-
-app.run()
-
+if __name__ == "__main__":
+    asyncio.run(main())
