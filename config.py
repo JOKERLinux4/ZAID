c7d58af0076187fcefbac3b780a58fa5d043fd9c
JOKERLinux4 <163691846+JOKERLinux4@users.noreply.github.com>
2025-08-05 23:31:02 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 29366148
+API_HASH = "cdae9daaa962cfdf1ec88c5333aadedf"
+SESSION = "هنا_يضع_المستخدم_جلسة_بايجرام"
+
+# معلومات المطور
+DEVELOPER_NAME = "قِٰـِۢــآئــد آلِٰـِۢــشِٰـِۢــيِٰـِۢـآطِٰـِۢـيِٰـِـنَ"
+DEVELOPER_USER = "@X_5NN"
+SOURCE_CHANNEL = "@C_9KK"
