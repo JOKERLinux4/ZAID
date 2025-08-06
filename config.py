817ed3c360b9879eb5b496e2b17c1cd6f098821b
JOKERLinux4 <163691846+JOKERLinux4@users.noreply.github.com>
2025-08-06 11:06:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 29366148
-API_HASH = "cdae9daaa962cfdf1ec88c5333aadedf"
-SESSION = "هنا_يضع_المستخدم_جلسة_بايجرام"
+API_ID = 12345678  # ضع API_ID هنا
+API_HASH = "xxxxxxxxxxxxxxxxxxxxxxxxxx"  # ضع API_HASH هنا
+SESSION = "جلسة_بايروجرام_هنا"
 
 DEVELOPER_NAME = "قِٰـِۢــآئــد آلِٰـِۢــشِٰـِۢــيِٰـِۢـآطِٰـِۢـيِٰـِـنَ"
 DEVELOPER_USER = "@X_5NN"
